19fac11f049da0b843d1d4dc6ce2cab7f8b08f29
bubs150 <81664743+bubs150@users.noreply.github.com>
2021-04-14 22:17:21 -0400

    Update authenticate.php

--- login/authenticate.php
+++ login/authenticate.php
@@ -1,5 +1,5 @@
 <?php
-    require(__DIR__ . "/../lib/navigate.php");
+    require("navigate.php");
     if(isset($_REQUEST["email"])){
         $email = $_REQUEST["email"];
         $password = $_REQUEST["password"];
