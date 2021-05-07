d8f88aa12733685df6b0e1035c29c46adff22000
bubs150 <81664743+bubs150@users.noreply.github.com>
2021-05-07 15:46:21 -0400

    Update game_settings.php

--- api/game_settings.php
+++ api/game_settings.php
@@ -2,11 +2,12 @@
 $response = [
 	"status" => 200,
 	"data" => [
+    "countdown"=>2,
 		"speed"=>5,
 		"targetLength"=>50,
 		"sideLength"=>50
 	]
-]
+];
 
 echo json_encode($response, JSON_PRETTY_PRINT);		
 ?>
